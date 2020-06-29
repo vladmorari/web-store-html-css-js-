20554dc7f1e20cece5380eec093d2cabf948d6a7
vladmorari <morariv67@gmail.com>
2020-06-29 11:35:54 +0300

    header added

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var img = document.createElement("img");
 img.src = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/4646c711677199.572bdaa658fc3.png";
-
 var src = document.getElementById("header-bg");
 src.appendChild(img);
 
-var x = 0;
+var count = 1;
 
 function addToBox() {
 
-    document.getElementById("cart-number").innerHTML = x;
-    x++;
+    document.getElementById("cart-number").innerHTML = count;
+    count++;
 }
